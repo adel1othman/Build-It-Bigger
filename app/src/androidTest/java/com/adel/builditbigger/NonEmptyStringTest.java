1a53c3f85dc3059771f99a49f81b66f7b4698dfb
Adel <adel.othman@live.com>
2018-08-11 16:31:50 +0200

    Fixed test case

--- app/src/androidTest/java/com/adel/builditbigger/NonEmptyStringTest.java
+++ app/src/androidTest/java/com/adel/builditbigger/NonEmptyStringTest.java
@@ -1,25 +1,38 @@
 package com.adel.builditbigger;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class NonEmptyStringTest extends AndroidTestCase {
 
+    private Context appContext = InstrumentationRegistry.getTargetContext();
     private static final String LOG_TAG = "NonEmptyStringTest";
 
-    @SuppressWarnings("unchecked")
+    @Test
     public void test() {
         Log.v("NonEmptyStringTest", "Running NonEmptyStringTest test");
-        String result = null;
-        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(getContext(), null);
+
+        boolean gettingAnEx = false;
+        String result;
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(appContext, null);
         endpointsAsyncTask.execute();
+
         try {
             result = endpointsAsyncTask.get();
             Log.d(LOG_TAG, "Retrieved a non-empty string successfully: " + result);
         } catch (Exception e) {
-            e.printStackTrace();
+            gettingAnEx = true;
+            result = e.getMessage();
         }
-        assertNotNull(result);
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result, !gettingAnEx);
     }
-
 }
